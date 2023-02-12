d330302be33fd9939deecce8c037c79de302e89d
munaffara <113467187+munaffara@users.noreply.github.com>
2023-02-12 17:08:49 +0530

    footer add

---
+++ footer.php
@@ -0,0 +1,77 @@
+<!-- start: FOOTER -->
+<footer class="footer">
+    <div class="container">
+        <!-- top footer statrs -->
+        <div class="row top-footer">
+            <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
+                <a href="#"> <img src="images/food-picky-logo.png" alt="Footer logo"> </a> <span>Choose it &amp; Enjoy your meals! </span> </div>
+            <div class="col-xs-12 col-sm-2 about color-gray">
+                <h5>About Us</h5>
+                <ul>
+                    <li><a href="#">Our Mission</a></li>
+                    <li><a href="#">Social Media</a></li>
+                    <!--<li><a href="#">Testimonials</a></li>
+                    <li><a href="#">We are hiring</a></li>-->
+					<li><a href="#">Join us Today</a></li>
+                </ul>
+            </div>
+            <!-- <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
+                <h5>How it Works?</h5>
+                <ul>
+                    <li><a href="#">Enter your location</a></li>
+                    <li><a href="#">Choose the restaurant</a></li>
+                    <li><a href="#">Choose your dishes</a></li>
+                    <li><a href="#">Get delivered</a></li>
+					<li><a href="#">Pay on delivery</a></li>
+					<li><a href="#">Enjoy your meals :)</a></li>
+                </ul>
+            </div> -->
+            <div class="col-xs-12 col-sm-2 pages color-gray">
+                <h5>Legal</h5>
+                <ul>
+                    <li><a href="#">Terms & Conditions</a> </li>
+                    <li><a href="#">Refund & Cancellation</a> </li>
+                    <li><a href="#">Privacy Policy</a> </li>
+                    <li><a href="#">Cookie Policy</a> </li>
+                    <li><a href="#">Offer Terms</a> </li>
+                </ul>
+            </div>
+
+            <div class="col-xs-12 col-sm-4 address color-gray">
+                    <h5>Address:</h5>
+                    <p>Pitipana junction, Homagama.</p>
+                    <div class="col-xs-12 col-sm-5 additional-info color-gray">
+            </div>
+            
+        </div>
+        <!-- top footer ends -->
+        <!-- bottom footer statrs -->
+        <div class="bottom-footer">
+            <div class="row">
+                <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                    <h5>Social Media</h5>
+                    <ul>
+                        <li>
+                            <i class="fa fa-facebook-f" style="font-size:24px;color:blue"></i>
+                        </li>
+                        <li>
+                            <i class="fa fa-twitter" style="font-size:24px;color:blue"></i>
+                        </li>
+                        <li>
+                            <i class="fa fa-whatsapp" style="font-size:24px;color:green"></i>
+                        </li>
+                        <li>
+                            <i class="fa fa-instagram" style="font-size:24px;color:red"></i>
+                        </li>
+                        <li>
+                            <i class="fa fa-envelope" style="font-size:24px;color:#0489c9"></i>
+                        </li>
+                    </ul>
+                </div>
+                
+            </div>
+        </div>
+        <!-- bottom footer ends -->
+    </div>
+</footer>
+<!-- end:Footer -->
--- registration.php
+++ registration.php
@@ -52,7 +52,7 @@ if(isset($_POST['submit'] )) //if submit btn is pressed
 	else{
        
 	 //inserting values into db
-	$mql = "INSERT INTO users(username,f_name,l_name,email,phone,password,address) VALUES('".$_POST['username']."','".$_POST['firstname']."','".$_POST['lastname']."','".$_POST['email']."','".$_POST['phone']."','".md5($_POST['password'])."','".$_POST['address']."')";
+	$mql = "INSERT INTO users(username,f_name,l_name,email,phone,password) VALUES('".$_POST['username']."','".$_POST['firstname']."','".$_POST['lastname']."','".$_POST['email']."','".$_POST['phone']."','".md5($_POST['password'])."')";
 	mysqli_query($db, $mql);
 		$success = "Account Created successfully! <p>You will be redirected in <span id='counter'>5</span> second(s).</p>
 					<script type='text/javascript'>
@@ -223,91 +223,11 @@ if(isset($_POST['submit'] )) //if submit btn is pressed
                 </div>
             </div>
         </section>
-        <!-- start: FOOTER -->
-        <footer class="footer">
-            <div class="container">
-                <!-- top footer statrs -->
-                <div class="row top-footer">
-                    <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
-                        <a href="#"> <img src="images/food-picky-logo.png" alt="Footer logo"> </a> <span>Choose it &amp; Enjoy your meals! </span> </div>
-                    <div class="col-xs-12 col-sm-2 about color-gray">
-                        <h5>About Us</h5>
-                        <ul>
-                            <li><a href="#">Our Mission</a></li>
-                            <li><a href="#">Social Media</a></li>
-                            <li><a href="#">Testimonials</a></li>
-                            <li><a href="#">We are hiring</a></li>
-							<li><a href="#">Join us Today</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
-                        <h5>How it Works?</h5>
-                        <ul>
-                            <li><a href="#">Enter your location</a></li>
-                            <li><a href="#">Choose the restaurant</a></li>
-                            <li><a href="#">Choose your dishes</a></li>
-                            <li><a href="#">Get delivered</a></li>
-							<li><a href="#">Pay on delivery</a></li>
-							<li><a href="#">Enjoy your meals :)</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-12 col-sm-2 pages color-gray">
-                        <h5>Legal</h5>
-                        <ul>
-                            <li><a href="#">Terms & Conditions</a> </li>
-                            <li><a href="#">Refund & Cancellation</a> </li>
-                            <li><a href="#">Privacy Policy</a> </li>
-                            <li><a href="#">Cookie Policy</a> </li>
-                            <li><a href="#">Offer Terms</a> </li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-12 col-sm-3 popular-locations color-gray">
-                        <h5>Locations We</h5>
-                        <ul>
-                            <li><a href="#">Jaffna</a> </li>
-                            <li><a href="#">Mannar</a> </li>
-                            <li><a href="#">Colombo</a> </li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- top footer ends -->
-                <!-- bottom footer statrs -->
-                <div class="bottom-footer">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-3 payment-options color-gray">
-                            <h5>All Major Credit Cards Accepted</h5>
-                            <ul>
-                                <li>
-                                    <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
-                                </li>
-                                <li>
-                                    <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
-                                </li>
-                                <li>
-                                    <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
-                                </li>
-                                <li>
-                                    <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
-                                </li>
-                                <li>
-                                    <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-xs-12 col-sm-4 address color-gray">
-                            <h5>Address:</h5>
-                            <p>Mahatma Gandhi Salai, Chennai - 600034.</p>
-                            <h5>Call us at: <a href="tel:+914450005500">+91 44 50005500</a></h5></div>
-                        <div class="col-xs-12 col-sm-5 additional-info color-gray">
-                            <h5>Who are we?</h5>
-                            <p>Launched in 2021, Our technology platform connects customers, restaurant partners and delivery partners, serving their multiple needs. Customers use our platform to search and discover restaurants, read and write customer generated reviews and view and upload photos, order food delivery, book a table and make payments while dining-out at restaurants.</p>
-                        </div>
-                    </div>
-                </div>
-                <!-- bottom footer ends -->
-            </div>
-        </footer>
-        <!-- end:Footer -->
+
+    <!-- start: FOOTER -->
+         <?php include('footer.php'); ?>
+    <!-- end:Footer -->
+
          </div>
          <!-- end:page wrapper -->
       
