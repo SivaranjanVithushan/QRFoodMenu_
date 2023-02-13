dedd46f486184964683479df0d6c604aec5dce1d
Svithushan <ict19868@fot.sjp.ac.lk>
2023-02-13 23:21:25 +0530

    change some display text

--- admin/update_menu.php
+++ admin/update_menu.php
@@ -7,50 +7,44 @@ error_reporting(0);
 session_start();
 
 
-if(isset($_POST['submit']))           //if upload btn is pressed
+if (isset($_POST['submit']))           //if upload btn is pressed
 {
-		if(empty($_POST['d_name'])||empty($_POST['about'])||$_POST['price']==''||$_POST['c_name']=='')
-		{
-											$error = 	'<div class="alert alert-danger alert-dismissible fade show">
+    if (empty($_POST['d_name']) || empty($_POST['about']) || $_POST['price'] == '' || $_POST['c_name'] == '') {
+        $error =     '<div class="alert alert-danger alert-dismissible fade show">
 																<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 																<strong>All fields Must be Fillup!</strong>
 															</div>';
-		}
-	else
-		{
-				$fname = $_FILES['file']['name'];
-								$temp = $_FILES['file']['tmp_name'];
-								$fsize = $_FILES['file']['size'];
-								$extension = explode('.',$fname);
-								$extension = strtolower(end($extension));
-								$fnew = uniqid().'.'.$extension;
+    } else {
+        $fname = $_FILES['file']['name'];
+        $temp = $_FILES['file']['tmp_name'];
+        $fsize = $_FILES['file']['size'];
+        $extension = explode('.', $fname);
+        $extension = strtolower(end($extension));
+        $fnew = uniqid() . '.' . $extension;
 
-								$store = "Res_img/dishes/".basename($fnew);                      // the path to store the upload image
+        $store = "Res_img/dishes/" . basename($fnew);                      // the path to store the upload image
 
-					if($extension == 'jpg'||$extension == 'png'||$extension == 'gif' )
-					{
-									if($fsize>=1000000)
-										{
-												$error = 	'<div class="alert alert-danger alert-dismissible fade show">
+        if ($extension == 'jpg' || $extension == 'png' || $extension == 'gif') {
+            if ($fsize >= 1000000) {
+                $error =     '<div class="alert alert-danger alert-dismissible fade show">
 																<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 																<strong>Max Image Size is 1024kb!</strong> Try different Image.
 															</div>';
-										}
-									else
-										{
-												$sql = "update food set c_id='$_POST[c_name]',title='$_POST[d_name]',slogan='$_POST[about]',price='$_POST[price]',img='$fnew' where d_id='$_GET[menu_upd]'";  // update the submited data ino the database :images
-												mysqli_query($db, $sql);
-												move_uploaded_file($temp, $store);
+            } else {
+                $sql = "update food set c_id='$_POST[c_name]',title='$_POST[d_name]',slogan='$_POST[about]',price='$_POST[price]',img='$fnew' where d_id='$_GET[menu_upd]'";  // update the submited data ino the database :images
+                mysqli_query($db, $sql);
+                move_uploaded_file($temp, $store);
 
-													$success = 	'<div class="alert alert-success alert-dismissible fade show">
+                $success =     '<div class="alert alert-success alert-dismissible fade show">
 																<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 																<strong>Record</strong>Updated.
 															</div>';
-							}
-					}
-	   }
+            }
+        }
+    }
 }
 ?>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -60,7 +54,7 @@ if(isset($_POST['submit']))           //if upload btn is pressed
     <meta name="author" content="">
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="images/foodpicky.png">
-    <title>FoodPicky - Admin Dashboard</title>
+    <title>Admin Dashboard</title>
     <!-- Bootstrap Core CSS -->
     <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
@@ -78,7 +72,8 @@ if(isset($_POST['submit']))           //if upload btn is pressed
     <!-- Preloader - style you can find in spinners.css -->
     <div class="preloader">
         <svg class="circular" viewBox="25 25 50 50">
-			<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
+            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
+        </svg>
     </div>
     <!-- Main wrapper  -->
     <div id="main-wrapper">
@@ -89,7 +84,7 @@ if(isset($_POST['submit']))           //if upload btn is pressed
                 <div class="navbar-header">
                     <a class="navbar-brand" href="index.php">
                         <!-- Logo icon -->
-                        <b><img src="images/foodpicky.png" alt="homepage" class="dark-logo" width = "25px"/></b>
+                        <b><img src="images/foodpicky.png" alt="homepage" class="dark-logo" width="25px" /></b>
                         <!--End Logo icon -->
                         <!-- Logo text -->
                         <span><b>Food Picky</b></span>
@@ -113,9 +108,11 @@ if(isset($_POST['submit']))           //if upload btn is pressed
                                         <!-- Contact -->
                                         <form>
                                             <div class="form-group">
-                                                <input type="text" class="form-control" id="exampleInputname1" placeholder="Enter Name"> </div>
+                                                <input type="text" class="form-control" id="exampleInputname1" placeholder="Enter Name">
+                                            </div>
                                             <div class="form-group">
-                                                <input type="email" class="form-control" placeholder="Enter email"> </div>
+                                                <input type="email" class="form-control" placeholder="Enter email">
+                                            </div>
                                             <div class="form-group">
                                                 <textarea class="form-control" id="exampleTextarea" rows="3" placeholder="Message"></textarea>
                                             </div>
@@ -133,18 +130,19 @@ if(isset($_POST['submit']))           //if upload btn is pressed
                         <!-- Search -->
                         <li class="nav-item hidden-sm-down search-box"> <a class="nav-link hidden-sm-down text-muted  " href="javascript:void(0)"><i class="ti-search"></i></a>
                             <form class="app-search">
-                                <input type="text" class="form-control" placeholder="Search here"> <a class="srh-btn"><i class="ti-close"></i></a> </form>
+                                <input type="text" class="form-control" placeholder="Search here"> <a class="srh-btn"><i class="ti-close"></i></a>
+                            </form>
                         </li>
-                       
-                      
+
+
                     </ul>
                 </div>
             </nav>
         </div>
         <!-- End header header -->
-      
+
         <!-- Left Sidebar  -->
-            <?php include('side_bar.php'); ?>
+        <?php include('side_bar.php'); ?>
         <!--End Left Sidebar -->
 
         <!-- Page wrapper  -->
@@ -152,7 +150,8 @@ if(isset($_POST['submit']))           //if upload btn is pressed
             <!-- Bread crumb -->
             <div class="row page-titles">
                 <div class="col-md-5 align-self-center">
-                    <h3 class="text-primary">Update Menu Info</h3> </div>
+                    <h3 class="text-primary">Update Menu Info</h3>
+                </div>
                 <div class="col-md-7 align-self-center">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="javascript:void(0)">Options</a></li>
@@ -164,95 +163,94 @@ if(isset($_POST['submit']))           //if upload btn is pressed
             <!-- Container fluid  -->
             <div class="container-fluid">
                 <!-- Start Page Content -->
-									<?php  echo $error;
-									        echo $success; ?>
-					    <div class="col-lg-12">
-                        <div class="card card-outline-primary">
-                            <div class="card-header">
-                                <h4 class="m-b-0 text-white">Update Menu for Restaurant</h4>
-                            </div>
-                            <div class="card-body">
-                                <form action='' method='post'  enctype="multipart/form-data">
-                                    <div class="form-body">
-                                        <?php $qml ="select * from food where d_id='$_GET[menu_upd]'";
-																				$rest=mysqli_query($db, $qml);
-																				$roww=mysqli_fetch_array($rest);
-																				?>
-                                        <hr>
-                                        <div class="row p-t-20">
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label class="control-label">Dish Name</label>
-                                                    <input type="text" name="d_name" value="<?php echo $roww['title'];?>" class="form-control" placeholder="Parotta">
-                                                   </div>
-                                            </div>
-                                            <!--/span-->
-                                            <div class="col-md-6">
-                                                <div class="form-group has-danger">
-                                                    <label class="control-label">Description</label>
-                                                    <input type="text" name="about" value="<?php echo $roww['slogan'];?>" class="form-control form-control-danger" placeholder="South indian special">
-                                                    </div>
-                                            </div>
-                                            <!--/span-->
-                                        </div>
-                                        <!--/row-->
-                                        <div class="row p-t-20">
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label class="control-label">Price </label>
-                                                    <input type="text" name="price" value="<?php echo $roww['price'];?>"  class="form-control" placeholder="&#8377;">
-                                                   </div>
-                                            </div>
-                                            <!--/span-->
-                                            <div class="col-md-6">
-                                                <div class="form-group has-danger">
-                                                    <label class="control-label">Image</label>
-                                                    <input type="file" name="file"  id="lastName" class="form-control form-control-danger" placeholder="12n">
-                                                    </div>
-                                            </div>
-                                        </div>
-                                        <!--/row-->
-
-                                        <!--/span-->
-                                        <div class="row">
-											 										<div class="col-md-12">
-                                                <div class="form-group">
-                                                    <label class="control-label">Select Restaurant</label>
-																										<select name="c_name" class="form-control custom-select" data-placeholder="Choose a Category" tabindex="1">
-                                                        <option>--Select any Restaurant--</option>
-                                                 				<?php $ssql ="select * from food_category";
-																												$res=mysqli_query($db, $ssql);
-																												while($row=mysqli_fetch_array($res))
-																												{
-															                             echo' <option value="'.$row['c_id'].'">'.$row['c_name'].'</option>';;
-																												}
-																												?>
-													 													</select>
-                                              </div>
-                                          </div>
-                                      </div>
-                                </div>
-                                    </div>
-                                    <div class="form-actions">
-                                        <input type="submit" name="submit" class="btn btn-success" value="Save">
-                                        <a href="dashboard.php" class="btn btn-inverse">Cancel</a>
-                                    </div>
-                                </form>
-                            </div>
+                <?php echo $error;
+                echo $success; ?>
+                <div class="col-lg-12">
+                    <div class="card card-outline-primary">
+                        <div class="card-header">
+                            <h4 class="m-b-0 text-white">Update Menu</h4>
                         </div>
+                        <div class="card-body">
+                            <form action='' method='post' enctype="multipart/form-data">
+                                <div class="form-body">
+                                    <?php $qml = "select * from food where d_id='$_GET[menu_upd]'";
+                                    $rest = mysqli_query($db, $qml);
+                                    $roww = mysqli_fetch_array($rest);
+                                    ?>
+                                    <hr>
+                                    <div class="row p-t-20">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label class="control-label">Dish Name</label>
+                                                <input type="text" name="d_name" value="<?php echo $roww['title']; ?>" class="form-control" placeholder="Parotta">
+                                            </div>
+                                        </div>
+                                        <!--/span-->
+                                        <div class="col-md-6">
+                                            <div class="form-group has-danger">
+                                                <label class="control-label">Description</label>
+                                                <input type="text" name="about" value="<?php echo $roww['slogan']; ?>" class="form-control form-control-danger" placeholder="South indian special">
+                                            </div>
+                                        </div>
+                                        <!--/span-->
+                                    </div>
+                                    <!--/row-->
+                                    <div class="row p-t-20">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label class="control-label">Price </label>
+                                                <input type="text" name="price" value="<?php echo $roww['price']; ?>" class="form-control" placeholder="&#8377;">
+                                            </div>
+                                        </div>
+                                        <!--/span-->
+                                        <div class="col-md-6">
+                                            <div class="form-group has-danger">
+                                                <label class="control-label">Image</label>
+                                                <input type="file" name="file" id="lastName" class="form-control form-control-danger" placeholder="12n">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!--/row-->
+
+                                    <!--/span-->
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label class="control-label">Select Category</label>
+                                                <select name="c_name" class="form-control custom-select" data-placeholder="Choose a Category" tabindex="1">
+                                                    <option>--Select any Category--</option>
+                                                    <?php $ssql = "select * from food_category";
+                                                    $food = mysqli_query($db, $ssql);
+                                                    while ($row = mysqli_fetch_array($food)) {
+                                                        echo ' <option value="' . $row['c_id'] . '">' . $row['c_name'] . '</option>';;
+                                                    }
+                                                    ?>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                        </div>
+                        <div class="form-actions">
+                            <input type="submit" name="submit" class="btn btn-success" value="Save">
+                            <a href="dashboard.php" class="btn btn-inverse">Cancel</a>
+                        </div>
+                        </form>
                     </div>
-
-
-
                 </div>
-                <!-- End PAge Content -->
             </div>
-            <!-- End Container fluid  -->
-            <!-- footer -->
-			<footer class="footer"> © 2023 All rights reserved. Made with &#x1F49C; by <b>Group-06</b> Team. </footer>
-            <!-- End footer -->
+
+
+
         </div>
-        <!-- End Page wrapper  -->
+        <!-- End PAge Content -->
+    </div>
+    <!-- End Container fluid  -->
+    <!-- footer -->
+    <footer class="footer"> © 2023 All rights reserved. Made with &#x1F49C; by <b>Group-06</b> Team. </footer>
+    <!-- End footer -->
+    </div>
+    <!-- End Page wrapper  -->
     </div>
     <!-- End Wrapper -->
     <!-- All Jquery -->
