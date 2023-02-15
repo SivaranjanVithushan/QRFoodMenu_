20e4f54503508576bcd0dddefcf41d64bd3b3397
Svithushan <ict19868@fot.sjp.ac.lk>
2023-02-15 20:19:38 +0530

    side_bar arange and delete add_restraunt.php and update_restraunt.php

--- admin/side_bar.php
+++ admin/side_bar.php
@@ -22,11 +22,9 @@
                         </li>
                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-cutlery" aria-hidden="true"></i><span class="hide-menu">Menu</span></a>
                             <ul aria-expanded="false" class="collapse">
-								<li><a href="all_menu.php">All Menues</a></li>
                                 <li><a href="add_category.php">Add Category</a></li>
 								<li><a href="add_menu.php">Add Menu</a></li>
-
-
+								<li><a href="all_menu.php">All Menues</a></li>
                             </ul>
                         </li>
 						 <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span class="hide-menu">Orders</span></a>
