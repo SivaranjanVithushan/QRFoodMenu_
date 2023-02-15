d772de632bde865080fad39e15a05ee984620c38
Svithushan <ict19868@fot.sjp.ac.lk>
2023-02-15 19:00:01 +0530

    change the content of chekout.php

--- checkout.php
+++ checkout.php
@@ -57,7 +57,7 @@ if (empty($_SESSION["user_id"])) {
                         <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
                             <ul class="nav navbar-nav">
                                 <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
-                                <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
+                                <!-- <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li> -->
 
                                 <?php
                                 if (empty($_SESSION["user_id"])) {
@@ -83,9 +83,9 @@ if (empty($_SESSION["user_id"])) {
                     <div class="container">
                         <ul class="row links">
 
-                            <li class="col-xs-12 col-sm-4 link-item"><span>1</span><a href="restaurants.php">Choose Restaurant</a></li>
+                            <li class="col-xs-12 col-sm-4 link-item"><span>1</span><a href="restaurants.php">Choose Category</a></li>
                             <li class="col-xs-12 col-sm-4 link-item "><span>2</span><a href="#">Pick your favourite dishes</a></li>
-                            <li class="col-xs-12 col-sm-4 link-item active"><span>3</span><a href="checkout.php">Get delivered & Pay</a></li>
+                            <li class="col-xs-12 col-sm-4 link-item active"><span>3</span><a href="checkout.php">Get Food & Pay</a></li>
                         </ul>
                     </div>
                 </div>
@@ -123,15 +123,15 @@ if (empty($_SESSION["user_id"])) {
 
                                                             <tr>
                                                                 <td>Cart Subtotal</td>
-                                                                <td> &#8377;<?php echo $item_total; ?></td>
+                                                                <td> Rs.<?php echo $item_total; ?></td>
                                                             </tr>
-                                                            <tr>
+                                                            <!-- <tr>
                                                                 <td>Shipping &amp; Handling</td>
                                                                 <td>FREE*</td>
-                                                            </tr>
+                                                            </tr> -->
                                                             <tr>
                                                                 <td class="text-color"><strong>Total</strong></td>
-                                                                <td class="text-color"><strong> &#8377;<?php echo $item_total; ?></strong></td>
+                                                                <td class="text-color"><strong>Rs. <?php echo $item_total; ?></strong></td>
                                                             </tr>
                                                         </tbody>
 
@@ -147,7 +147,8 @@ if (empty($_SESSION["user_id"])) {
                                                     <li>
                                                         <label class="custom-control custom-radio  m-b-20">
                                                             <input name="mod" id="radioStacked1" checked value="COD" type="radio" class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Cash on delivery</span>
-                                                            <br> <span>Pay digitally with SMS Pay Link. Cash may not be accepted in COVID restricted areas.</span> </label>
+                                                            <br> <span>you can Pay digitally with SMS Pay Link.</span> 
+                                                        </label>
                                                     </li>
                                                     <li>
                                                         <label class="custom-control custom-radio  m-b-10">
@@ -164,128 +165,8 @@ if (empty($_SESSION["user_id"])) {
             </div>
             </form>
         </div>
-        <section class="app-section">
-            <div class="app-wrap">
-                <div class="container">
-                    <div class="row text-img-block text-xs-left">
-                        <div class="container">
-                            <div class="col-xs-12 col-sm-5 right-image text-center">
-                                <figure> <img src="images/app.png" alt="Right Image" class="img-fluid"> </figure>
-                            </div>
-                            <div class="col-xs-12 col-sm-7 left-text">
-                                <h3>Food Picky - The Best Food Delivery App</h3>
-                                <p>Got Hungry? Get the food you want, from the restaurants you love, delivered at blinking speed.
-
-                                    Eat what you like, where you like, when you like. Find the local flavours you crave, all at the tap of a button.</p>
-                                <div class="social-btns">
-                                    <a href="#" class="app-btn apple-button clearfix">
-                                        <div class="pull-left"><i class="fa fa-apple"></i> </div>
-                                        <div class="pull-right"> <span class="text">Available on the</span> <span class="text-2">App Store</span> </div>
-                                    </a>
-                                    <a href="#" class="app-btn android-button clearfix">
-                                        <div class="pull-left"><i class="fa fa-android"></i> </div>
-                                        <div class="pull-right"> <span class="text">Available on the</span> <span class="text-2">Play Store</span> </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
         <!-- start: FOOTER -->
-        <footer class="footer">
-            <div class="container">
-                <!-- top footer statrs -->
-                <div class="row top-footer">
-                    <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
-                        <a href="#"> <img src="images/food-picky-logo.png" alt="Footer logo"> </a> <span>Choose it &amp; Enjoy your meals! </span>
-                    </div>
-                    <div class="col-xs-12 col-sm-2 about color-gray">
-                        <h5>About Us</h5>
-                        <ul>
-                            <li><a href="#">Our Mission</a></li>
-                            <li><a href="#">Social Media</a></li>
-                            <li><a href="#">Testimonials</a></li>
-                            <li><a href="#">We are hiring</a></li>
-                            <li><a href="#">Join us Today</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
-                        <h5>How it Works?</h5>
-                        <ul>
-                            <li><a href="#">Enter your location</a></li>
-                            <li><a href="#">Choose the restaurant</a></li>
-                            <li><a href="#">Choose your dishes</a></li>
-                            <li><a href="#">Get delivered</a></li>
-                            <li><a href="#">Pay on delivery</a></li>
-                            <li><a href="#">Enjoy your meals :)</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-12 col-sm-2 pages color-gray">
-                        <h5>Legal</h5>
-                        <ul>
-                            <li><a href="#">Terms & Conditions</a> </li>
-                            <li><a href="#">Refund & Cancellation</a> </li>
-                            <li><a href="#">Privacy Policy</a> </li>
-                            <li><a href="#">Cookie Policy</a> </li>
-                            <li><a href="#">Offer Terms</a> </li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-12 col-sm-3 popular-locations color-gray">
-                        <h5>Locations We Deliver To</h5>
-                        <ul>
-                            <li><a href="#">Chennai</a> </li>
-                            <li><a href="#">Kanchipuram</a> </li>
-                            <li><a href="#">Tiruchy</a> </li>
-                            <li><a href="#">Salem</a> </li>
-                            <li><a href="#">Madurai</a> </li>
-                            <li><a href="#">Theni</a> </li>
-                            <li><a href="#">Thiruvallur</a> </li>
-                            <li><a href="#">Pondicherry</a> </li>
-                            <li><a href="#">Thoothukudi</a> </li>
-                            <li><a href="#">Kanyakumari</a> </li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- top footer ends -->
-                <!-- bottom footer statrs -->
-                <div class="bottom-footer">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-3 payment-options color-gray">
-                            <h5>All Major Credit Cards Accepted</h5>
-                            <ul>
-                                <li>
-                                    <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
-                                </li>
-                                <li>
-                                    <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
-                                </li>
-                                <li>
-                                    <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
-                                </li>
-                                <li>
-                                    <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
-                                </li>
-                                <li>
-                                    <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-xs-12 col-sm-4 address color-gray">
-                            <h5>Address:</h5>
-                            <p>Mahatma Gandhi Salai, Chennai - 600034.</p>
-                            <h5>Call us at: <a href="tel:+914450005500">+91 44 50005500</a></h5>
-                        </div>
-                        <div class="col-xs-12 col-sm-5 additional-info color-gray">
-                            <h5>Who are we?</h5>
-                            <p>Launched in 2021, Our technology platform connects customers, restaurant partners and delivery partners, serving their multiple needs. Customers use our platform to search and discover restaurants, read and write customer generated reviews and view and upload photos, order food delivery, book a table and make payments while dining-out at restaurants.</p>
-                        </div>
-                    </div>
-                </div>
-                <!-- bottom footer ends -->
-            </div>
-        </footer>
+        <?php include('footer.php'); ?>
         <!-- end:Footer -->
         </div>
         <!-- end:page wrapper -->
