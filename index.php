2669a10be98f1d05450eca338ca1cc1cfe478353
Svithushan <ict19868@fot.sjp.ac.lk>
2023-02-15 18:58:19 +0530

    change the name of the nav bar

--- index.php
+++ index.php
@@ -35,12 +35,12 @@ session_start(); //start temp session until logout/browser closed
                 <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
 
                 <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/food-picky-logo.png" alt=""> </a>
-
+                
 
                 <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
                     <ul class="nav navbar-nav">
                         <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
-                        <li class="nav-item"> <a class="nav-link active" href="dishes.php">Dishes<span class="sr-only"></span></a> </li>
+                        <li class="nav-item"> <a class="nav-link active" href="dishes.php">Add Cart <span class="sr-only"></span></a> </li>
 
                         <?php
                         if (empty($_SESSION["user_id"])) // if user is not login
